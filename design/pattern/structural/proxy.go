4d06f8fd004c87b34129db5491d4f57aa6e5b060
sir <hammercui@163.com>
2023-01-29 18:09:37 +0800

    feat: proxy pattern

---
+++ design/pattern/structural/proxy.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+/**
+结构型模式
+代理模式
+1 可用作cache layer
+2 或者增强层，比如access控制
+ */
+type Vehicle interface {
+	Drive()
+}
+
+type Car struct {
+	
+}
+func (c *Car) Drive() {
+	fmt.Println("Car is being driven")
+}
+
+type Driver struct {
+	Age int
+}
+
+type CarProxy struct {
+	driver *Driver
+	vehicle Vehicle
+}
+
+func (c *CarProxy) Drive() {
+	if c.driver.Age > 18{
+		c.vehicle.Drive()
+	}else {
+		fmt.Println("Driver too young!")
+	}
+}
+
+func NewCarProxy(driver *Driver) *CarProxy{
+	return &CarProxy{driver: driver,vehicle: &Car{}}
+}
+
+
+
+
---
+++ design/pattern/structural/proxy_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "testing"
+
+func TestProxy(t *testing.T) {
+	carProxy := NewCarProxy(&Driver{Age: 12})
+	carProxy.Drive()
+
+
+	carProxy2 := NewCarProxy(&Driver{Age: 19})
+	carProxy2.Drive()
+}
