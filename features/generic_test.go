f8e0ac369d60d8d5a5450f8311e5e9ab5049bb82
sir <hammercui@163.com>
2023-02-14 15:27:23 +0800

    feat: generic contact lint

--- features/generic_test.go
+++ features/generic_test.go
@@ -7,27 +7,28 @@ import (
 
 /**
 泛型测试
- */
+*/
 
-func TestFindIndex(t *testing.T)  {
+func TestFindIndex(t *testing.T) {
 
 	sel := 1
-	arr := []int{1,2,3,4,5}
+	arr := []int{1, 2, 3, 4, 5}
 	pos := 0
-	if pos == findIndex(arr, sel){
+	if pos == findIndex(arr, sel) {
 		t.Log("success")
-	}else{
+	} else {
 		t.Fatal("findIndex error")
 	}
 }
 
 func TestContacts(t *testing.T) {
-
-	a := []int{1,2}
-	b := []int{3,4}
+	a := []int{1, 2}
+	b := []int{3, 4}
 	fmt.Println(concatenate(a, b))
-
-	c := []string{"a","b"}
-	d := []string{"c","d"}
-	fmt.Println(concatenate(c,d))
 }
+
+// func TestContactsLint(t *testing.T) {
+// 	c := []string{"a", "b"}
+// 	d := []string{"c", "d"}
+// 	fmt.Println(concatenate(c, d))
+// }
